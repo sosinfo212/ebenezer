f0c61f37cf1a6c6f68f24c07cd687910c123a082
pcdesign2023 <138577223+pcdesign2023@users.noreply.github.com>
2024-08-02 17:26:19 +0100

    integrate UI

--- crm/routes/web.php
+++ crm/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/{page}', [AdminController::class, 'index']);
